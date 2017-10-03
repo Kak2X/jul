3357e166297d2ce33aeb7da4fb73c0463a1eaa23
Kak <kak32x@gmail.com>
2017-10-04 00:44:08 +0200

    clean up activity.php
    (and fix a SQL injection I accidentaly introduced previously. oopsies)

--- activity.php
+++ activity.php
@@ -1,27 +1,36 @@
 <?php
 	require 'lib/function.php';
 
-	$u = filter_string($_GET['u']);
-	$user['regdate'] = $sql->resultq("SELECT regdate FROM users WHERE id='$u'") or die();
-
-	$vd=date('m-d-y', $user['regdate']);
-	$dd=mktime(0,0,0,substr($vd,0,2),substr($vd,3,2),substr($vd,6,2));
-
-	$days = floor((ctime()-$dd)/86400);
-	if (!$days) die();
-	$pq = $sql->fetchq(
-		"SELECT FROM_UNIXTIME(date, '%Y-%m-%d') day, count(*) c ".
-		"FROM posts WHERE user={$u} GROUP BY day ORDER BY day", PDO::FETCH_KEY_PAIR, mysql::FETCH_ALL);
-	for($i=0; $i <= $days; ++$i) {
-        $dk = date('Y-m-d',$dd+$i*86400);
-		if (!array_key_exists($dk, $pq)) continue;
-		$p[$i] = $pq[$dk];
+	$_GET['u'] = filter_int($_GET['u']);
+	
+	// Get timestamp of the *day* the user registered (remove hour/min/sec)
+	$regdate = $sql->resultq("SELECT regdate FROM users WHERE id = {$_GET['u']}") or die("User doesn't exist.");
+	$regday     = floor($regdate / 86400); // Day number
+	$regdate_ts = $regday * 86400;         // Day timestamp
+	
+	$days = floor((ctime()-$regdate)/86400); // Account age (in days)
+	if (!$days) die("Account age too low.");
+	
+	// A faster (?) way to group them; by using the day number instead of getting a date string
+	$postdts = $sql->fetchq("
+		SELECT FLOOR(date / 86400) day, COUNT(*) c 
+		FROM posts 
+		WHERE user = {$_GET['u']} 
+		GROUP BY day 
+		ORDER BY day
+	", PDO::FETCH_KEY_PAIR, mysql::FETCH_ALL);
+	
+	if (!$postdts) die("No posts for this user.");
+	
+	// Place the results into an array with a saner format [<day number> => <count>]
+	foreach ($postdts as $k_day => $posts) {
+		$day = $k_day - $regday;
+		$postdb[$day] = $posts;
 	}
-	
-	if (!isset($p)) die();
-	
-	$m=max($p);
-	$img=ImageCreate($days,$m);
+	unset($postdts, $k_day, $posts);
+
+	$maxposts = max($postdb);
+	$img = ImageCreateTrueColor($days, $maxposts);
 
 	$c['bg']  = ImageColorAllocate($img,  0,  0,  0);
 	$c['bg1'] = ImageColorAllocate($img,  0,  0, 80); // Month colors
@@ -30,27 +39,37 @@
 	$c['mk1'] = ImageColorAllocate($img,110,110,160); // Horizontal Rulers
 	$c['mk2'] = ImageColorAllocate($img, 70, 70,130); //
 	$c['bar'] = ImageColorAllocate($img,240,190, 40); // Post count bar
-	$c['pt1']  = ImageColorAllocate($img,250,250,250); // Average
-	$c['pt2']  = ImageColorAllocate($img,240,230,220); // Average (over top of post bar)
+	$c['pt1'] = ImageColorAllocate($img,250,250,250); // Average
+	$c['pt2'] = ImageColorAllocate($img,240,230,220); // Average (over top of post bar)
 
-	for($i=0;$i<$days;++$i){
-		$num=date('m',$dd+$i*86400)%2+1;
-		if(date('m-d',$dd+$i*86400)=='01-01') $num=3;
-		ImageLine($img,$i,$m,$i,0,$c["bg$num"]);
-	}
-	for($i=50, $ct=1; $i<=$m; $i+=50, ++$ct)
-		ImageLine($img,0,$m-$i,$days,$m-$i,(($ct&1) ? $c['mk2'] : $c['mk1']));
-
-	$pt=0;
-	for($i=0;$i<$days;++$i) {
-		if (isset($p[$i])) {
-			ImageLine($img,$i,$m,$i,$m-$p[$i],$c['bar']);
-			$pt += $p[$i];
+	// Draw the background
+	for ($i = 0; $i < $days; ++$i) {
+		$md = date('m-d', $regdate_ts + $i * 86400);
+		if ($md == '01-01') { // New year?
+			$num = 3;
 		} else {
-			$p[$i] = 0;
+			$num = substr($md, 0, 2) % 2 + 1; // Alternate between months
 		}
-		$avg = $pt/($i+1);
-		ImageSetPixel($img,$i,$m-$avg,(($p[$i] >= $avg) ? $c['pt2'] : $c['pt1']));
+		ImageLine($img,$i,$maxposts,$i,0,$c["bg$num"]);
+	}
+	
+	// Draw the horizontal rulers (in offsets of Y 50px; alternate lines between 2 colors)
+	for ($y = 50; $y <= $maxposts; $y += 50) {
+		ImageLine($img, 0, $maxposts - $y, $days, $maxposts - $y, $c['mk'.($y%100+1)]);
+	}
+	
+	$total = 0;
+	for($i = 0; $i < $days;++$i) {
+		// Draw the number of posts (as vertical bars starting from the bottom)
+		if (isset($postdb[$i])) {
+			ImageLine($img, $i, $maxposts, $i, $maxposts - $postdb[$i], $c['bar']);
+			$total += $postdb[$i];
+		} else {
+			$postdb[$i] = 0;
+		}
+		// Draw post average in relation to time
+		$avg = $total / ($i + 1);
+		ImageSetPixel($img, $i, $maxposts - $avg, (($postdb[$i] >= $avg) ? $c['pt2'] : $c['pt1']));
 	}
 
 	Header('Content-type:image/png');

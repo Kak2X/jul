a035e32ed0846745cdf91449a0cc01d7c70c423a
Kak <kak32x@gmail.com>
2018-01-20 01:03:19 +0100

    PHP 5.5 compat and adding status messages for filter editor

--- admin-editfilters.php
+++ admin-editfilters.php
@@ -4,37 +4,50 @@
 	
 	admin_check();	
 	
-	$_GET['id'] 	= filter_int($_GET['id']);
-	$_GET['type'] 	= filter_int($_GET['type']);
-	$_GET['sort']	= filter_bool($_GET['sort']); // Sort by type (0) or forum (1). will change type column to forum in preview mode
+	$filter_types = array(
+		1 => "Generic",
+		2 => "URLs",
+		3 => "HTML/CSS",
+		4 => "Bad words",
+		5 => "Joke/Idiocy",
+		6 => "Hidden Smilies",
+		7 => "Security",
+		//99 => "Test"
+	);
+	$max_filters = count($filter_types);
+	
+	$_GET['id']     = filter_int($_GET['id']);
+	$_GET['type']   = numrange(filter_int($_GET['type']), 0, $max_filters);
 	
 	if (isset($_POST['setdel']) && isset($_POST['del'])) {
+		// Delete multiple filters (from the overview page)
 		check_token($_POST['auth']);
 		
 		if (is_array($_POST['del'])) {
 			$q = $sql->prepare("DELETE FROM filters WHERE id = ?");
-			foreach ($_POST['del'] as $delid) {
-				$delid = (int) $delid;
-				if ($delid > 0) {
-					$sql->execute($q, [$delid]);
-				}
+			$i = 0;
+			foreach ($_POST['del'] as $delid) { 
+				$sql->execute($q, [$delid]); // The value is already safe as is, no need for further checks
+				++$i;
 			}
+			msg_holder::set_cookie("{$i} filter".($i != 1 ? 's' : '')." deleted.");
 		}
-		header("Location: admin-editfilters.php?type={$_GET['type']}");
-		die;		
+		return header("Location: ?type={$_GET['type']}");	
 	} else if (isset($_POST['qdelid'])) {
+		// Delete a single filter (from the edit page)
 		check_token($_POST['auth']);
+		
 		$delid = filter_int($_POST['qdelid']);
 		if ($delid > 0) {
-			$sql->query("DELETE FROM filters WHERE id = $delid");
+			$sql->query("DELETE FROM filters WHERE id = {$delid}");
+			msg_holder::set_cookie("1 filter deleted.");
 		}
-		header("Location: admin-editfilters.php?type={$_GET['type']}");
-		die;
+		return header("Location: admin-editfilters.php?type={$_GET['type']}");
 	} else if (isset($_POST['edit'])) {
 		check_token($_POST['auth']);
 		
-		$source 		= filter_string($_POST['source']);
-		$forum			= filter_int($_POST['forum']);
+		$source         = filter_string($_POST['source']);
+		$forum          = filter_int($_POST['forum']);
 		if ($forum && !$sql->resultq("SELECT 1 FROM forums WHERE id = $forum")) {
 			$forum = 0;
 		}
@@ -42,10 +55,17 @@
 			errorpage("No source string specified.");
 		}
 		
+		$type = numrange(filter_int($_POST['type']), 1, $max_filters);
+		if ($type != $_POST['type']) {
+			errorpage("Invalid type selected.");
+		}
+		
 		if  ($_GET['id'] <= -1) {
 			$q = "INSERT INTO filters SET ".mysql::setplaceholders('source','replacement','comment','method','forum','enabled','type');
+			msg_holder::set_cookie("Added a filter to '{$filter_types[$type]}'.");
 		} else {
 			$q = "UPDATE filters SET ".mysql::setplaceholders('source','replacement','comment','method','forum','enabled','type')." WHERE id = {$_GET['id']}";
+			msg_holder::set_cookie("Edited filter from '{$filter_types[$type]}'.");
 		}
 		
 		$sql->queryp($q, 
@@ -56,11 +76,10 @@
 			'method'      => numrange(filter_int($_POST['method']), 0, 3),
 			'forum'       => $forum,
 			'enabled'     => filter_int($_POST['enabled']),
-			'type'        => numrange(filter_int($_POST['type']), 1, 7),
+			'type'        => $type,
 		]);
 		
-		header("Location: admin-editfilters.php?type={$_GET['type']}");
-		die;
+		return header("Location: ?type={$_GET['type']}");
 	}
 
 
@@ -68,63 +87,50 @@
 	pageheader("Board Filters");
 	
 	print adminlinkbar();
+	print msg_holder::get_message();
 	
-	const FILTER_TYPES = array(
-		1 => "Generic",
-		2 => "URLs",
-		3 => "HTML/CSS",
-		4 => "Bad words",
-		5 => "Joke/Idiocy",
-		6 => "Hidden Smilies",
-		7 => "Security",
-	);		
-
+	// Layout stuff
+	$hl = array();
 	
-
+	$hl['filter-title'] = ($_GET['type'] ? " - ".$filter_types[$_GET['type']] : "");
+	
+	$hl['filter-sel'] = "";
+	foreach ($filter_types as $i => $filter_name) {
+		$hl['filter-sel'] .= "<a href='?type={$i}'>{$filter_name}</a><br>";
+	}
 
 ?>
 <style type="text/css">
 	.rh {height: 19px;}
-	.va {vertical-align: top}
+	textarea {display: block}
 </style>
 <form method="POST" action="?type=<?= $_GET['type'] ?>&id=<?= $_GET['id'] ?>">
-<input type="hidden" name="auth" value="<?= generate_token() ?>">
+<?= auth_tag() ?>
+
 <table class="table">
 	<tr>
-		<td class="tdbgh center" style="width: 120px">
-			<b>Filter types</b>
+		<td class="tdbgh center b" style="width: 120px">
+			Filter types
 		</td>
-		<td class="tdbgh center" colspan=7>
-			<b>Filters<?= ($_GET['type'] ? " - ".FILTER_TYPES[$_GET['type']] : "") ?></b>
+		<td class="tdbgh center b" colspan=7>
+			Filters<?= $hl['filter-title'] ?>
 		</td>
 	</tr>
 	<tr class="rh">
-		<td class="tdbg1 nobr va" rowspan=999999>
-			<a href="?type=1"><?= FILTER_TYPES[1] ?></a><br>
-			<a href="?type=2"><?= FILTER_TYPES[2] ?></a><br>
-			<a href="?type=3"><?= FILTER_TYPES[3] ?></a><br>
-			<a href="?type=4"><?= FILTER_TYPES[4] ?></a><br>			
-			<a href="?type=5"><?= FILTER_TYPES[5] ?></a><br>
-			<a href="?type=6"><?= FILTER_TYPES[6] ?></a><br>
-			<a href="?type=7"><?= FILTER_TYPES[7] ?></a>
+		<td class="tdbg1 nobr vatop" rowspan=999999>
+			<?= $hl['filter-sel'] ?>
 		</td>
 
-<?php
-			
-			
-		if (!$_GET['type']) {
-?>
+<?php	if (!$_GET['type']) { ?>
 		<td class="tdbg1 center rh">Select a filter type.</td>
 	</tr>
 	<tr><td class="tdbg2 center">&nbsp;</td></tr>
-
-	
 <?php
 	
 		} else {
-			
 			// Edit / New filter
 			if ($_GET['id']) {
+				//////////////////////////////////////
 				if ($_GET['id'] <= -1) {
 					$editAction = "Creating a new filter";
 					$sel_method[2] = 'selected';
@@ -146,38 +152,40 @@
 				}
 				
 ?>
-		<td class="tdbgc center" colspan=7><b><?= $editAction ?></b></td>
+		<td class="tdbgc center b" colspan=7><?= $editAction ?></td>
 	</tr>
 	
 	<tr class="rh">
-		<td class="tdbgh center" rowspan=3 colspan=2><b>Search for:</b></td>
-		<td class="tdbg2 va" rowspan=3 colspan=2>
-			<textarea wrap=virtual name="source" ROWS=3 maxlength=127 style="width: 100%; resize:vertical"><?= htmlspecialchars($x['source']) ?></textarea>
+		<td class="tdbgh center b" rowspan=3 colspan=2>Search for:</td>
+		<td class="tdbg2 vatop" rowspan=3 colspan=2>
+			<textarea wrap=virtual name="source" ROWS=3 maxlength=127 style="width: 100%; resize:vertical"><?= 
+				htmlspecialchars($x['source']) 
+			?></textarea>
 		</td>
-		<td class="tdbgh center" colspan=3><b>Options</b></td>
+		<td class="tdbgh center b" colspan=3>Options</td>
 	</tr>
 	
 	<tr class="rh">
 		<td class="tdbg1" colspan=3>
 			<input type="checkbox" id="enabled" name="enabled" value=1 <?= ($x['enabled'] ? "checked" : "") ?>>
 			<label for="enabled">Enabled</label>
-<?php	if ($_GET['id'] > 0) {	?>	
-			<span style="float: right"><input type="checkbox" name="qdelid" value="<?= $x['id'] ?>"> Delete&nbsp;</span>
-<?php	} ?>
+		<?php	if ($_GET['id'] > 0) {	?>	
+					<span style="float: right"><input type="checkbox" name="qdelid" value="<?= $x['id'] ?>"> Delete&nbsp;</span>
+		<?php	} ?>
 		</td>
 	</tr>
 	
 	<tr class="rh">
-		<td class="tdbgh center"><b>Forum:</b></td>
+		<td class="tdbgh center b">Forum:</td>
 		<td class="tdbg1" colspan=2><?= doforumlist((int) $x['forum'], 'forum', '[Global Filter]') ?></td>
 	</tr>
 	
 	<tr class="rh">
-		<td class="tdbgh center" rowspan=3 colspan=2><b>Replace with:</b></td>
-		<td class="tdbg2 va" rowspan=3 colspan=2>
+		<td class="tdbgh center b" rowspan=3 colspan=2>Replace with:</td>
+		<td class="tdbg2 vatop" rowspan=3 colspan=2>
 			<textarea wrap=virtual name="replacement" ROWS=3 maxlength=127 style="width: 100%; resize:vertical"><?= htmlspecialchars($x['replacement']) ?></textarea>
 		</td>
-		<td class="tdbgh center"><b>Method:</b></td>
+		<td class="tdbgh center b">Method:</td>
 		<td class="tdbg1" colspan=2>
 			<select name="method">
 				<option value=0 <?= filter_string($sel_method[0]) ?>>Case sensitive replacement</option>
@@ -187,14 +195,12 @@
 		</td>
 	</tr>
 	<tr class="rh">
-		<td class="tdbgh center"><b>Type:</b></td>
+		<td class="tdbgh center b">Type:</td>
 		<td class="tdbg1" colspan=2>
 			<select name='type'>
-				<?php
-				for ($i = 1, $m = count(FILTER_TYPES); $i <= $m; ++$i) {
-					echo "<option value=$i ".filter_string($sel_type[$i]).">".FILTER_TYPES[$i]."</option>";
-				}
-				?>
+			<?php for ($i = 1, $m = count($filter_types); $i <= $m; ++$i) { ?>
+				<option value="<?= $i ?>" <?= filter_string($sel_type[$i]) ?>> <?= $filter_types[$i] ?></option>
+			<?php }	?>
 			</select>
 		</td>
 	</tr>
@@ -207,8 +213,8 @@
 	<tr><td class="tdbg2" colspan=7></td></tr>
 	
 	<tr class="rh">
-		<td class="tdbgh center" colspan=2><b>Comment:</b></td>
-		<td class="tdbg2 va" colspan=5>
+		<td class="tdbgh center b" colspan=2>Comment:</td>
+		<td class="tdbg2 vatop" colspan=5>
 			<textarea wrap=virtual name="comment" ROWS=1 maxlength=255 style="width: 100%; resize:vertical"><?= htmlspecialchars($x['comment']) ?></textarea>
 		</td>
 	</tr>
@@ -216,34 +222,36 @@
 	<tr><td class="tdbgh" colspan=7></td></tr>
 	<tr class="rh">
 
-<?php	} ?>
+<?php 			//////////////////////////////////////
+			} ?>
 
-		<td class="tdbgc center" style="width: 60px">&nbsp;</td>
-		<td class="tdbgc center" style="width: 40px"><b>Set</b></td>
-		<td class="tdbgc center" style="width: 350px"><b>Search</b></td>
-		<td class="tdbgc center" style="width: 350px"><b>Replacement</b></td>
-		<td class="tdbgc center" style="width: 150px"><b>Forum</b></td>
-		<td class="tdbgc center" style="width: 230px"><b>Method</b></td>
-		<td class="tdbgc center" style="width: 10px"><b>?</b></td>
+		<td class="tdbgc center b" style="width: 60px">&nbsp;</td>
+		<td class="tdbgc center b" style="width: 40px">Set</td>
+		<td class="tdbgc center b" style="width: 350px">Search</td>
+		<td class="tdbgc center b" style="width: 350px">Replacement</td>
+		<td class="tdbgc center b" style="width: 150px">Forum</td>
+		<td class="tdbgc center b" style="width: 230px">Method</td>
+		<td class="tdbgc center b" style="width: 10px">?</td>
 		
 	</tr>
 <?php
-			// Filter list
-			$filters = $sql->query("
-				SELECT f.id, f.method, f.enabled, f.source, f.replacement, f.comment, x.title ftitle, x.id fid
-				FROM filters f
-				LEFT JOIN forums x ON f.forum = x.id
-				WHERE f.type = {$_GET['type']}
-				ORDER BY f.source ASC
-			");
-			for ($i = 0; $x = $sql->fetch($filters); ++$i) {	
-				#################
+
+		// Filter list
+		$filters = $sql->query("
+			SELECT f.id, f.method, f.enabled, f.source, f.replacement, f.comment, x.title ftitle, x.id fid
+			FROM filters f
+			LEFT JOIN forums x ON f.forum = x.id
+			WHERE f.type = {$_GET['type']}
+			ORDER BY f.source ASC
+		");
+		for ($i = 0; $x = $sql->fetch($filters); ++$i) {	
+			#################
 ?>
 	<tr class="rh">
 		<td class="tdbg1 center fonts">
 			<input type="checkbox" name="del[]" value=<?= $x['id'] ?>> - <a href="?type=<?= $_GET['type'] ?>&id=<?= $x['id'] ?>">Edit</a>
 		</td>
-			<td class="tdbg1 center"><b><span style=color:<?= ($x['enabled'] ? "#0F0>ON" : "#F00>OFF") ?></b></span>
+			<td class="tdbg1 center b"><span style=color:<?= ($x['enabled'] ? "#0F0>ON" : "#F00>OFF") ?></span>
 		</td>
 		<td class="tdbg2"><textarea wrap=virtual ROWS=1 style="width: 100%; resize:none" readonly><?= htmlentities($x['source']) ?></textarea></td>
 		<td class="tdbg2"><textarea wrap=virtual ROWS=1 style="width: 100%; resize:none" readonly><?= htmlspecialchars($x['replacement']) ?></textarea></td>
@@ -256,8 +264,8 @@
 		</td>
 	</tr>
 <?php
-				################
-			}
+			################
+		}
 ?>
 	<tr><td class="tdbg2 center" colspan=7>&nbsp;</td></tr>
 
@@ -265,11 +273,7 @@
 		<td class="tdbgc" style="border-right: 0" colspan=2><input type="submit" style="height: 16px; font-size: 10px" name="setdel" value="Delete Selected"></td>
 		<td class="tdbgc center" colspan=5><a href="?type=<?= $_GET['type'] ?>&id=-1">&lt; Add a new filter &gt;</a></td>
 	</tr>
-<?php
-		}
-
-		
-			?>
+<?php }	?>
 		</td>
 	</tr>
 </table>

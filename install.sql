d640379f88f7eb89468a7184a2520bfa1dfe76f1
Kak <kak32x@gmail.com>
2018-02-23 21:35:30 +0100

    correct an minor issue from the sql file

--- install.sql
+++ install.sql
@@ -627,7 +627,7 @@ CREATE TABLE `forums` (
 --
 
 INSERT INTO `forums` (`id`, `title`, `description`, `olddesc`, `catid`, `minpower`, `minpowerthread`, `minpowerreply`, `numthreads`, `numposts`, `lastpostdate`, `lastpostuser`, `lastpostid`, `forder`, `specialscheme`, `hidden`, `specialtitle`, `pollstyle`) VALUES
-(1, 'General Forum', 'For everybody.', '', 1, 0, 0, 0, 13, 21, 1517771941, 1, 24, 1, NULL, 0, '', 0),
+(1, 'General Forum', 'For everybody.', '', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, NULL, 0, '', 0),
 (2, 'General Staff Forum', 'Not for everybody.', '', 2, 1, 1, 1, 0, 0, 0, 0, 0, 2, NULL, 0, '', 0),
 (3, 'Trash Forum', '?', '', 1, 0, 2, 2, 0, 0, 0, 0, 0, 2, NULL, 0, '', 0);
 

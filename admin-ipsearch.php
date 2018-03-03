a165dcf19223b288e5e2413411e8d1ce5ab8d951
Kak <kak32x@gmail.com>
2018-03-03 22:32:57 +0100

    add experimental pagination to ipsearch page
    I mean it's better than cutting off the results completely

--- admin-ipsearch.php
+++ admin-ipsearch.php
@@ -1,29 +1,53 @@
 <?php
-	require 'lib/function.php';
-	pageheader("IP Address Search");
-	admincheck();
-	print adminlinkbar();
 
-	$_POST['ip'] = filter_string($_POST['ip']);
+	require 'lib/function.php';
+	
+	admincheck();
+	
+	pageheader("IP Address Search");
+	print adminlinkbar();
+	
+	
+	if (isset($_GET['ip'])) {
+		$_POST['ip'] = $_GET['ip'];
+	} else {
+		$_POST['ip'] = filter_string($_POST['ip']);
+	}
+	
+	// Why the fuck did this truncate results instead of using pagination?
+	if (!isset($_POST['ppp'])) {
+		$_POST['ppp'] = 500;
+	} else {
+		$_POST['ppp'] = numrange(filter_int($_POST['ppp']), 1, 500);
+	}
+	$_POST['page'] = filter_int($_POST['page']);
+
 	if(!filter_string($_POST['su'])) $_POST['su']='n';
 	if(!filter_string($_POST['sp'])) $_POST['sp']='u';
 	if(!filter_string($_POST['sm'])) $_POST['sm']='n';
-	if(!filter_string($_POST['y']))  $_POST['d'] ='y';
-	$ch1[$_POST['su']]= ' checked';
-	$ch2[$_POST['sp']]= ' checked';
-	$ch3[$_POST['sm']]= ' checked';
-	$ch4[$_POST['d']] = ' checked';
+	if(!filter_string($_POST['d']))  $_POST['d'] ='y';
+	$ch1[$_POST['su']]   = ' checked';
+	$ch2[$_POST['sp']]   = ' checked';
+	$ch3[$_POST['sm']]   = ' checked';
+	$ch4[$_POST['d']]    = ' checked';
+	$ch5[$_POST['ppp']]  = ' selected';
+	$ch6[$_POST['page']] = ' selected';
+	
+	
 
 ?>
-<form action='?' method=post>
+<form method="POST" action="?">
 <table class='table'>
 	<tr><td class='tdbgh center' colspan=2>IP search</td></tr>
 	<tr>
-		<td class='tdbg1 center' style="width: 20%"><b>IP to search:</b></td>
-		<td class='tdbg2'><input type='text' name=ip size=15 maxlength=15 value="<?=htmlspecialchars($_POST['ip'])?>"></td>
+		<td class='tdbg1 center b' style="width: 20%">IP to search:</td>
+		<td class='tdbg2'>
+			<input type="text" name="ip" size="32" maxlength="32" value="<?=htmlspecialchars($_POST['ip'])?>">
+			<span class='fonts'>use * as wildcard</span>
+		</td>
 	</tr>
 	<tr>
-		<td class='tdbg1 center'><b>Sort users by:</b></td>
+		<td class='tdbg1 center b'>Sort users by:</td>
 		<td class='tdbg2'>
 			<input type=radio class='radio' name=su value=n<?=filter_string($ch1['n'])?>> Name &nbsp; &nbsp;
 			<input type=radio class='radio' name=su value=p<?=filter_string($ch1['p'])?>> Posts &nbsp; &nbsp;
@@ -34,7 +58,7 @@
 		</td>
 	</tr>
 	<tr>
-		<td class='tdbg1 center'><b>Sort posts by:</b></td>
+		<td class='tdbg1 center b'>Sort posts by:</td>
 		<td class='tdbg2'>
 			<input type=radio class='radio' name=sp value=u<?=filter_string($ch2['u'])?>> User &nbsp; &nbsp;
 			<input type=radio class='radio' name=sp value=d<?=filter_string($ch2['d'])?>> Date &nbsp; &nbsp;
@@ -42,7 +66,7 @@
 		</td>
 	</tr>
 	<tr>
-		<td class='tdbg1 center'><b>Sort private messages by:</b></td>
+		<td class='tdbg1 center b'>Sort private messages by:</td>
 		<td class='tdbg2'>
 			<input type=radio class='radio' name=sm value=n<?=filter_string($ch3['n'])?>> Sent by &nbsp; &nbsp;
 			<input type=radio class='radio' name=sm value=d<?=filter_string($ch3['d'])?>> Date &nbsp; &nbsp;
@@ -50,30 +74,42 @@
 		</td>
 	</tr>
 	<tr>
-		<td class='tdbg1 center'><b>Distinct users and IP's:</b></td>
+		<td class='tdbg1 center b'>Distinct users and IP's:</td>
 		<td class='tdbg2'>
 			<input type=radio class='radio' name=d value=y<?=filter_string($ch4['y'])?>> Yes &nbsp; &nbsp;
 			<input type=radio class='radio' name=d value=n<?=filter_string($ch4['n'])?>> No
 		</td>
 	</tr>
 	<tr>
+		<td class='tdbg1 center b'>Display:</td>
+		<td class='tdbg2'>
+			<select name='ppp'>
+				<option value='1'<?=filter_string($ch5['1'])?>>1</option>
+				<option value='50'<?=filter_string($ch5['50'])?>>50</option>
+				<option value='100'<?=filter_string($ch5['100'])?>>100</option>
+				<option value='250'<?=filter_string($ch5['250'])?>>250</option>
+				<option value='500'<?=filter_string($ch5['500'])?>>500</option>
+				<option value='1000'<?=filter_string($ch5['1000'])?>>1000</option>	
+			</select> entries
+		</td>
+	</tr>
+	<tr>
 		<td class='tdbg1 center'>&nbsp;</td>
-		<td class='tdbg1'><input type='submit' class=submit name=s value=Submit></td>
+		<td class='tdbg1'><input type='submit' class="submit" value="Submit"></td>
 	</tr>
 </table>
-</form>
 <?php
 
 	if ($_POST['ip']) {
 		$_POST['ip'] = str_replace('*', '%', $_POST['ip']);
 		
 		switch ($_POST['su']) {
-			case 'n': $usort='ORDER BY name'; break;
-			case 'p': $usort='ORDER BY posts DESC'; break;
-			case 'r': $usort='ORDER BY regdate'; break;
-			case 's': $usort='ORDER BY lastposttime'; break;
-			case 'a': $usort='ORDER BY lastactivity'; break;
-			case 'i': $usort='ORDER BY lastip'; break;
+			case 'n': $usort='ORDER BY u.name'; break;
+			case 'p': $usort='ORDER BY u.posts DESC'; break;
+			case 'r': $usort='ORDER BY u.regdate'; break;
+			case 's': $usort='ORDER BY u.lastposttime'; break;
+			case 'a': $usort='ORDER BY u.lastactivity'; break;
+			case 'i': $usort='ORDER BY u.lastip'; break;
 		}
 		switch ($_POST['sp']) {
 			case 'u': $psort='ORDER BY u.name'; break;
@@ -81,17 +117,24 @@
 			case 'i': $psort='ORDER BY p.ip'; break;
 		}
 		switch ($_POST['sm']) {
-			case 'n': $msort='ORDER BY u.name'; break; // name1
+			case 'n': $msort='ORDER BY u1.name'; break;
 			case 'd': $msort='ORDER BY p.date'; break;
 			case 'i': $msort='ORDER BY p.ip'; break;
 		}
 		if ($_POST['d'] === 'y') {
 			$pgroup='GROUP BY p.ip,u.id';
-			$mgroup='GROUP BY p.ip,u.id'; // u1
+			$mgroup='GROUP BY p.ip,u1.id';
 		} else {
 			$pgroup=$mgroup='';
 		}
-		$users = $sql->queryp("SELECT * FROM users WHERE lastip LIKE ? $usort", [$_POST['ip']]);
+		$users = $sql->queryp("
+			SELECT u.*, i.ip ipbanned
+			FROM users u 
+			LEFT JOIN ipbans i ON u.lastip = i.ip
+			WHERE u.lastip LIKE ? $usort
+			LIMIT ".($_POST['ppp'] * $_POST['page']).", {$_POST['ppp']}
+		", [$_POST['ip']]);
+		
 		$posts = $sql->queryp("
 			SELECT p.*, $userfields uid, t.title
 			FROM posts p
@@ -99,38 +142,48 @@
 			LEFT JOIN threads t ON p.thread = t.id 
 			WHERE p.ip LIKE ?
 			$pgroup $psort
+			LIMIT ".($_POST['ppp'] * $_POST['page']).", {$_POST['ppp']}
 		", [$_POST['ip']]);
-		// u1.name, u AS name1,u2.name AS name2,u1.sex AS sex1,u2.sex AS sex2,u1.powerlevel pow1,u2.powerlevel pow2 
-		// AND p.userto=u2.id
-		// bah
+		
 		$pmsgs = $sql->queryp("
-			SELECT p.id, p.userfrom, p.userto, p.title, p.ip, p.date, $userfields uid
+			SELECT 	p.id, p.userfrom, p.userto, p.title, p.ip, p.date,
+					u1.name, u1.sex, u1.`powerlevel`, u1.aka, u1.birthday, u1.namecolor,
+					u2.name, u2.sex, u2.`powerlevel`, u2.aka, u2.birthday, u2.namecolor
 			FROM pmsgs p
-			LEFT JOIN users u ON p.userfrom = u.id
+			LEFT JOIN users u1 ON p.userfrom = u1.id
+			LEFT JOIN users u2 ON p.userto   = u2.id
 			WHERE p.ip LIKE ?
 			$mgroup $msort
+			LIMIT ".($_POST['ppp'] * $_POST['page']).", {$_POST['ppp']}
 		", [$_POST['ip']]);
 
-
-		
+		// Somewhat wonky _POST pagination system, but better than nothing for now
+		$pagectrl = "";
+		if ($_POST['page'] > 0) {
+			$pagectrl .= "<button type='submit' class='submit' name='page' value='".($_POST['page']-1)."'>&lt;-</button>";
+		} else {
+			$pagectrl .= "<button type='submit' class='submit' disabled>&lt;-</button>";
+		}
+		$pagectrl = "<br>
+		<table class='table'><tr><td class='tdbg1 center'>
+		{$pagectrl} &mdash; <button type='submit' class='submit' name='page' value='".($_POST['page']+1)."'>-&gt;</button>
+		</td></tr></table>";
 		
 ?>
+<?= $pagectrl ?>
 <br>
 <table class='table'>
 	<tr>
-		<td class='tdbgh center' colspan=7><b>Users: <?=$sql->num_rows($users)?></b><tr>
+		<td class='tdbgh center' colspan=8><b>Users: <?=$sql->num_rows($users)?></b><tr>
 		<td class='tdbgc center'>id</td>
 		<td class='tdbgc center'>Name</td>
 		<td class='tdbgc center'>Registered on</td>
 		<td class='tdbgc center'>Last post</td>
 		<td class='tdbgc center'>Last activity</td>
 		<td class='tdbgc center'>Posts</td>
-		<td class='tdbgc center'>Last IP</td>
+		<td class='tdbgc center' colspan=2>Last IP</td>
 	</tr>
-<?php
-		for($c=0; $c<500 && $user=$sql->fetch($users); ++$c) {
-			//  if ($users['id'] != 428)
-?>
+<?php for ($c = 0; $user = $sql->fetch($users); ++$c) { ?>
 	<tr>
 		<td class='tdbg2 center'><?=$user['id']?></td>
 		<td class='tdbg1 center'><?=getuserlink($user)?></td>
@@ -139,19 +192,17 @@
 		<td class='tdbg1 center'><?=printdate($user['lastactivity'])?></td>
 		<td class='tdbg1 center'><?=$user['posts']?></td>
 		<td class='tdbg2 center'><?=$user['lastip']?></td>
+		<td class='tdbg2 center'><?=
+			($user['ipbanned'] ? 
+			"<a href='admin-ipbans.php?searchip={$user['lastip']}'>[IP BANNED]</a>" : 
+			"<a href='admin-ipbans.php?newip={$user['lastip']}#addban'>IP Ban</a>")?>
+		</td>
 	</tr>
-<?php
-		}
-		if($post = $sql->fetch($users))
-			print "<tr><td class='tdbg2 center' colspan=7>Too many results!</td></tr>";
-?>
+<?php } ?>
 </table>
 <br>
-<?php
 
 
-
-?>
 <table class='table'>
 	<tr>
 		<td class='tdbgh center' colspan=5><b>Posts: <?=$sql->num_rows($posts)?></b><tr>
@@ -161,10 +212,7 @@
 		<td class='tdbgc center'>Date</td>
 		<td class='tdbgc center'>IP</td>
 	</tr>
-<?php
-		for($c=0; $c<500 && $post=$sql->fetch($posts); ++$c) {
-		//if ($post['user'] != 428)
-?>
+<?php for($c = 0; $post = $sql->fetch($posts); ++$c) { ?>
 	<tr>
 		<td class='tdbg2 center'><?=$post['id']?></td>
 		<td class='tdbg1 center'><?=getuserlink($post, $post['user'])?></td>
@@ -172,22 +220,14 @@
 		<td class='tdbg1 center nobr'><?=printdate($post['date'])?></td>
 		<td class='tdbg2 center'><?=$post['ip']?></td>
 	</tr>
-<?php
-		}
-		if($post=$sql->fetch($posts))
-			print "<tr><td class='tdbg2 center' colspan=5>Too many results!</td></tr>";
-
-?>
+<?php }	?>
 </table>
 <br>
-<?php
 
 
-
-?>
 <table class='table'>
 	<tr>
-		<td class='tdbgh center' colspan=6><b>Private messages: <?=$sql->num_rows($pmsgs)?></b><tr>
+		<td class='tdbgh center b' colspan=6>Private messages: <?=$sql->num_rows($pmsgs)?><tr>
 		<td class='tdbgc center'>id</td>
 		<td class='tdbgc center'>Sent by</td>
 		<td class='tdbgc center'>Sent to</td>
@@ -196,24 +236,24 @@
 		<td class='tdbgc center'>IP</td>
 	</tr>
 <?php
-		for($c=0; $c<500 && $pmsg=$sql->fetch($pmsgs); ++$c) {
-			//if ($pmsg['userfrom'] != 428 && $pmsg['userto'] != 428)
+		for($c = 0; $pmsg = $sql->fetch($pmsgs, PDO::FETCH_NAMED); ++$c) {
+			if ($loguser['id'] == 1 || ($pmsg['userfrom'] != 1 && $pmsg['userto'] != 1)) {
 ?>
 	<tr>
 		<td class='tdbg2 center'><?=$pmsg['id']?></td>
-		<td class='tdbg1 center'><?=getuserlink($pmsg, $pmsg['userfrom'])?></td>
-		<td class='tdbg1 center'><?=getuserlink(NULL, $pmsg['userto'])?></td>
+		<td class='tdbg1 center'><?=getuserlink(array_column_by_key($pmsg, 0), $pmsg['userfrom'])?></td>
+		<td class='tdbg1 center'><?=getuserlink(array_column_by_key($pmsg, 1), $pmsg['userto'])?></td>
 		<td class='tdbg1 center'><a href="showprivate.php?id=<?=$pmsg['id']?>"><?=htmlspecialchars($pmsg['title'])?></a></td>
 		<td class='tdbg1 center nobr'><?=printdate($pmsg['date'])?></td>
 		<td class='tdbg2 center'><?=$pmsg['ip']?></td>
 	</tr>
 <?php	
+			}
 		}
-		if($pmsg=$sql->fetch($pmsgs))
-			print "<tr><td class='tdbg2 center' colspan=6>Too many results!</td></tr>";
 
 ?>
 </table>
+</form>
 <?php
 	}
 

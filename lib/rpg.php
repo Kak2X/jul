0542096f131e3d13cd338ab9ec788e6bcaa1e5c6
Kak <kak32x@gmail.com>
2018-03-08 09:36:08 +0100

    fix any errors in the rpg handler

--- lib/rpg.php
+++ lib/rpg.php
@@ -1,42 +1,61 @@
 <?php
 $stat = array('HP','MP','Atk','Def','Int','MDf','Dex','Lck','Spd');
-function basestat($p,$d,$stat){
-	$p+=0;
-	$e=calcexp($p,$d);
-	$l=calclvl($e);
-	if($l=='NAN') return 1;
-	switch($stat){
-		case 0: return (pow($p,0.26) * pow($d,0.08) * pow($l,1.41) * 0.95) + 20; //HP
-		case 1: return (pow($p,0.22) * pow($d,0.12) * pow($l,1.41) * 0.32) + 10; //MP
-		case 2: return (pow($p,0.18) * pow($d,0.04) * pow($l,1.37) * 0.29) +  2; //Str
-		case 3: return (pow($p,0.16) * pow($d,0.07) * pow($l,1.37) * 0.28) +  2; //Atk
-		case 4: return (pow($p,0.15) * pow($d,0.09) * pow($l,1.37) * 0.29) +  2; //Def
-		case 5: return (pow($p,0.14) * pow($d,0.10) * pow($l,1.37) * 0.29) +  1; //Shl
-		case 6: return (pow($p,0.17) * pow($d,0.05) * pow($l,1.37) * 0.29) +  2; //Lck
-		case 7: return (pow($p,0.19) * pow($d,0.03) * pow($l,1.37) * 0.29) +  1; //Int
-		case 8: return (pow($p,0.21) * pow($d,0.02) * pow($l,1.37) * 0.25) +  1; //Spd
+function basestat($posts, $days, $stat) {
+	static $basedefs = NULL;
+	$exp   = calcexp($posts, $days);
+	$level = calclvl($exp);
+	
+	if($level === 'NaN'){
+		return 1;
 	}
+	
+	if ($basedefs === NULL) {
+		$basedefs = array(
+	//   stat => [posts pow, days pow, lvl pow, multiplier, base val] 
+			0 => [     0.26,     0.08,    1.11,       0.95,       20], //HP
+			1 => [     0.22,     0.12,    1.11,       0.32,       10], //MP
+			2 => [     0.18,     0.04,    1.09,       0.29,        2], //Str
+			3 => [     0.16,     0.07,    1.09,       0.28,        2], //Atk
+			4 => [     0.15,     0.09,    1.09,       0.29,        2], //Def
+			5 => [     0.14,     0.10,    1.09,       0.29,        1], //Shl
+			6 => [     0.17,     0.05,    1.09,       0.29,        2], //Lck
+			7 => [     0.19,     0.03,    1.09,       0.29,        1], //Int
+			8 => [     0.21,     0.02,    1.09,       0.25,        1], //Spd
+		);
+	}
+	
+	return
+		  pow($posts, $basedefs[$stat][0]) // posts pow
+		* pow( $days, $basedefs[$stat][1]) // days pow
+		* pow($level, $basedefs[$stat][2]) // level pow
+		* $basedefs[$stat][3]              // base multiplier
+		+ $basedefs[$stat][4];             // base stat value
 }
-function getstats($u, $items=0, $class = 0){
+function getstats($user, $items = array(), $class = array()){
 	global $stat;
-	$p = $u['posts'];
-	$d = (ctime()-$u['regdate'])/86400;
-	for($i=0; $i<9; ++$i) {
-		$m[$i] = 1;
-	}
-	$a = array_fill(0, 9, 0);
-	for($i = 1; $i < 7; ++$i) {
-		$item = filter_int($items[$u['eq'.$i]]);
-		for($k = 0; $k < 9; ++$k){
-			$is = $item["s{$stat[$k]}"];
-			if (substr($item['stype'],$k,1)=='m') $m[$k] *= $is / 100;
-			else $a[$k] += $is;
+	
+	$posts = $user['posts'];
+	$days  = (ctime() - $user['regdate']) / 86400;
+	$m     = array_fill(0, 9, 1); // initialize multipliers to *1
+	$a     = array_fill(0, 9, 0); // initialize additions to +0
+	
+	foreach ($items as $item) {
+		for ($k = 0; $k < 9; ++$k) {
+			$statval = $item["s{$stat[$k]}"];
+			// stype is a string containing a sequence of 'm' and 'a'
+			if ($item['stype'][$k] === 'm') 
+				$m[$k] *= $statval / 100; // fixed point
+			else 
+				$a[$k] += $statval;
 		}
 	}
-	for($i = 0; $i < 9; ++$i){
-		$stats[$stat[$i]] = max(1, floor(basestat($p, $d, $i) * $m[$i]) + $a[$i]);
+	
+	// Calculate all the stats
+	for ($i = 0; $i < 9; ++$i) {
+		$stats[$stat[$i]] = max(1, floor(basestat($posts, $days, $i) * $m[$i]) + $a[$i]);
 	}
-	// after calculating stats with items
+	
+	// ...and their stat boost from classes
 	for($k = 0; $k < 9; ++$k) {
 		if (isset($class[$stat[$k]])) {
 			//$stats[$stat[$k]]	= ceil($stats[$stat[$k]] * ($class[$stat[$k]] != 0 ? $class[$stat[$k]] : -1));		// 0 can be 0, anything else will result in 1 because of max(1)
@@ -44,51 +63,52 @@ function getstats($u, $items=0, $class = 0){
 		}
 	}
 
-	$stats['GP'] 	= coins($p,$d)-$u['spent'];
-	$stats['exp'] 	= calcexp($p,$d);
-	$stats['lvl'] 	= calclvl($stats['exp']);
+	$stats['GP']     = coins($posts, $days) - $user['spent'];
+	$stats['exp']    = calcexp($posts, $days);
+	$stats['lvl']    = calclvl($stats['exp']);
 	return $stats;
 }
-function coins($p,$d){
-	$p+=0;
-	if($p<0 || $d<0) return 0;
-	return floor(pow($p,1.3) * pow($d,0.4) + $p*10);
+function coins($posts, $days) {
+	$posts += 0;
+	if ($posts < 0 || $days < 0) return 0;
+	else return floor(pow($posts, 1.3) * pow($days, 0.4) + $posts * 10);
+}
+function calcexpgainpost($posts, $days)	{
+	return floor(1.5 * sqrt($posts * $days));
 }
 
-/*
-	case 0: return (pow($p,0.21) * pow($d,0.15) * pow($l,1.11) * 1.00) + 20; //HP
-	case 1: return (pow($p,0.10) * pow($d,0.26) * pow($l,1.11) * 0.32) + 10; //MP
-	case 2: return (pow($p,0.16) * pow($d,0.09) * pow($l,1.09) * 0.29) +  2; //Str
-	case 3: return (pow($p,0.15) * pow($d,0.11) * pow($l,1.09) * 0.28) +  2; //Atk
-	case 4: return (pow($p,0.10) * pow($d,0.17) * pow($l,1.09) * 0.29) +  2; //Def
-	case 5: return (pow($p,0.09) * pow($d,0.18) * pow($l,1.09) * 0.29) +  1; //Shl
-	case 6: return (pow($p,0.13) * pow($d,0.13) * pow($l,1.09) * 0.29) +  2; //Lck
-	case 7: return (pow($p,0.07) * pow($d,0.20) * pow($l,1.09) * 0.29) +  1; //Int
-	case 8: return (pow($p,0.19) * pow($d,0.07) * pow($l,1.09) * 0.25) +  1; //Spd
-*/
-function calcexpgainpost($posts,$days)	{return @floor(1.5*@pow($posts*$days,0.5));}
-function calcexpgaintime($posts,$days)	{return sprintf('%01.3f',172800*@(@pow(@($days/$posts),0.5)/$posts));}
-
-function calcexpleft($exp)			{return calclvlexp(calclvl($exp)+1)-$exp;}
-function totallvlexp($lvl)			{return calclvlexp($lvl+1)-calclvlexp($lvl);}
-
-function calclvlexp($lvl){
-  if($lvl==1) return 0;
-  else return floor(pow(abs($lvl),3.5))*($lvl>0?1:-1);
+function calcexpgaintime($posts, $days)	{
+	if ($posts == 0) return '0.000'; // no division by 0
+	// <2 days> * (sqrt(days/posts) / posts)
+	else return sprintf('%01.3f', 172800 * (sqrt($days / $posts) / $posts));
 }
-function calcexp($posts,$days){
-  if(@($posts/$days)>0) return floor($posts*pow($posts*$days,0.5));
-  elseif($posts==0) return 0;
-  else return 'NaN';
+
+function calcexpleft($exp) {return calclvlexp(calclvl($exp) + 1) - $exp;}
+function totallvlexp($lvl) {return calclvlexp($lvl + 1) - calclvlexp($lvl);}
+
+function calclvlexp($lvl) {
+	if ($lvl == 1) return 0;
+	else return floor(pow(abs($lvl), 3.5)) * ($lvl > 0 ? 1 : -1);
+}
+function calcexp($posts, $days) {
+	if (!$posts || !$days) return 0;
+	else if ($posts > 0)   return floor($posts * sqrt($posts * $days));
+	else                   return 'NaN'; // Negative posts (likely a banned user)
 }
 function calclvl($exp){
-  if($exp>=0){
-    $lvl=floor(@pow($exp,2/7));
-    if(calclvlexp($lvl+1)==$exp) $lvl++;
-    if(!$lvl) $lvl=1;
-  }else $lvl=-floor(pow(-$exp,2/7));
-  if(is_string($exp) && $exp=='NaN') $lvl='NaN';
-  return $lvl;
+	if ($exp === 'NaN') {
+		$lvl = $exp;
+	} else if ($exp >= 0) {
+		$lvl = floor(pow($exp, 2 / 7));
+		// If we have enough exp for the next level, increase it
+		if (calclvlexp($lvl + 1) == $exp) ++$lvl;
+		else if (!$lvl)                   $lvl = 1;
+	} else {
+		// just in case, handle negative experience
+		// even though normally it should be impossible to have it
+		$lvl = -floor(pow(-$exp, 2 / 7));
+	}
+	return $lvl;
 }
 function getuseritems($user, $name = false, $extra = 0){
 	global $sql;
@@ -98,9 +118,6 @@ function getuseritems($user, $name = false, $extra = 0){
 	foreach($num as $i){
 		$q .= "r.eq$i = i.id OR ";
 	}	
-	//for ($i = 1, $q = ''; $i < 8; ++$i) {
-	//	$q .= "r.eq$i = i.id OR ";
-	//}
 	
 	// For our convenience we group this
 	$itemdb = $sql->fetchq("

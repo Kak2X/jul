21def4827db6e218f92b7e7e182a5961c51c78e5
Kak <kak32x@gmail.com>
2018-09-29 19:34:40 +0200

    Add new features to the layout preview page. - Soft refresh for CSS changes (including auto update mode) - Thread layout options - Sidebar options - Reorganizing the layout of the edit window.

--- postlayouts.php
+++ postlayouts.php
@@ -2,7 +2,10 @@
 
 	require "lib/function.php";
 	
-	$_GET['id'] = filter_int($_GET['id']);
+	$_GET['id']          = filter_int($_GET['id']);
+	$_COOKIE['plp_aupd'] = filter_int($_COOKIE['plp_aupd']);
+	
+	
 	if (!$_GET['id']) {
 		errorpage("No user selected.");
 	}
@@ -17,15 +20,22 @@
 	
 	
 	if (isset($_POST['submit'])) {
-		$user['postheader'] = filter_string($_POST['postheader']);
-		$user['signature']  = filter_string($_POST['signature']);
-		$user['css']        = filter_string($_POST['css']);
+		$user['postheader']  = filter_string($_POST['postheader']);
+		$user['signature']   = filter_string($_POST['signature']);
+		$user['css']         = filter_string($_POST['css']);
+		$user['sidebar']     = filter_string($_POST['sidebar']);
+		$user['sidebartype'] = numrange((filter_int($_POST['sidebartype']) << 1) + filter_int($_POST['sidebarcell']), 0, 5);
+		$loguser['layout']   = filter_int($_POST['tlayout']);
+	} else {
+		// Force extended layout by default
+		$loguser['layout'] = 6;
 	}
 	
 	// So that the layout shows up
 	$loguser['viewsig'] = 1;
 	$blockedlayouts = array();
 	
+
 	
 	$data = array(
 		// Text
@@ -51,57 +61,126 @@
 		//'attach_sel' => $attachsel,
 	);
 	
+	
+	// Sidebar options.
+	// Copied from the edit profile page.
+	$sidecell = $user['sidebartype'] & 1;
+	$sidetype = $user['sidebartype'] >> 1;		
+	$sidebartype = "
+	<style>
+		.pl_left{padding-left:20px;}
+		.pl_title{font-weight: bold;}
+	</style>
+	<div class='pl_title'>Sidebar type:</div>
+	<div class='pl_left'>
+	<input name='sidebartype' type='radio' value=0".($sidetype == 0 ? " checked" : "").">Normal<br>
+	<input name='sidebartype' type='radio' value=1".($sidetype == 1 ? " checked" : "").">Without options<br>
+	".(file_exists("sidebars/{$_GET['id']}.php") ? "<input name='sidebartype' type='radio' value=2".($sidetype == 2 ? " checked" : "").">PHP Code<br>" : "")."
+	</div>
+	<div class='pl_title'>Cell count:</div>
+	<div class='pl_left'>
+	<input name='sidebarcell' type='radio' value=0".($sidecell == 0 ? " checked" : "").">Two cell (default)<br>
+	<input name='sidebarcell' type='radio' value=1".($sidecell == 1 ? " checked" : "").">Single cell<br>
+	</div>";
+	
+	
+	// Thread layouts
+	$tlayoutq = $sql->query("SELECT id, name FROM tlayouts ORDER BY ord ASC, id ASC");
+	$tlayouts = "";
+	while ($x = $sql->fetch($tlayoutq)) {
+		$tlayouts .= "<option value='{$x['id']}'".($x['id'] == $loguser['layout'] ? " selected" : "").">{$x['name']}</option>";
+	}
+	
 ?>
 <form method="POST" action="?id=<?= $_GET['id'] ?>">
-<?= preview_post($user, $data, PREVIEW_EDITED) ?>
+<div id="postpreview">
+<?= preview_post($user, $data, PREVIEW_EDITED, getuserlink($user)."'s post layout") ?>
+</div>
 <table class="table">
-	<tr><td class="tdbgh center b" colspan=2><?= getuserlink($user) ?>'s post layout</td></tr>
+	<tr><td class="tdbgh center b" colspan=2>CSS</td></tr>
 	<tr>
-		<td class="tdbg1 center b">CSS:</td>
-		<td class="tdbg1 vatop">
-			<textarea wrap="virtual" id="css" name="css" rows=8 cols=<?=$numcols?> style="resize:vertical; min-height: 150px"><?= htmlspecialchars($user['css']) ?></textarea>
+		<td class="tdbg1 vatop" colspan=2>
+			<textarea id="css" name="css" rows=10 style="resize:vertical; width: 100%"><?= htmlspecialchars($user['css']) ?></textarea>
 		</td>
 	</tr>
+	
 	<tr>
-		<td class="tdbg1 center b">Header:</td>
-		<td class="tdbg1 vatop">
-			<textarea wrap="virtual" id="postheader" name="postheader" rows=8 cols=<?=$numcols?> style="resize:vertical; min-height: 150px"><?= htmlspecialchars($user['postheader']) ?></textarea>
-		</td>
+		<td class="tdbgh center b" style="width: 50%">Header</td>
+		<td class="tdbgh center b" style="width: 50%">Signature</td>
 	</tr>
 	<tr>
-		<td class="tdbg1 center b">Signature:</td>
 		<td class="tdbg1 vatop">
-			<textarea wrap="virtual" id="signature" name="signature" rows=8 cols=<?=$numcols?> style="resize:vertical; min-height: 150px"><?= htmlspecialchars($user['signature']) ?></textarea>
+			<textarea id="postheader" name="postheader" rows=4 style="resize:vertical; width: 100%"><?= htmlspecialchars($user['postheader']) ?></textarea>
+		</td>
+		<td class="tdbg1 vatop">
+			<textarea id="signature" name="signature" rows=4 style="resize:vertical; width: 100%"><?= htmlspecialchars($user['signature']) ?></textarea>
 		</td>
 	</tr>
+	
 	<tr>
-		<td class="tdbg1"></td>
-		<td class="tdbg1">
-			<!--<span id="jsbtn"><input type="button" value="Preview CSS" onclick="quickpreview()"> - </span>-->
-			<input type="submit" name="submit" value="Preview All">
+		<td class="tdbgh center b">Sidebar code (regular/extended only)</td>
+		<td class="tdbgh center b">Sidebar options (regular/extended only)</td>
+	</tr>
+	<tr>
+		<td class="tdbg1 vatop">
+			<?= ($sidetype == 2 && file_exists("sidebars/{$_GET['id']}.php")
+			? "<div style='background: #fff; overflow: scroll; width: 50vw; height: 400px; resize: vertical'>".highlight_file("sidebars/{$_GET['id']}.php", true)."</div><div style='display: none'>"
+			: "<div>") ?>
+				<textarea id="signature" name="sidebar" rows=8 style="resize:vertical; width: 100%"><?= htmlspecialchars($user['sidebar']) ?></textarea>
+			</div>
+		</td>
+		<td class="tdbg1 vatop">
+			<?= $sidebartype ?>
+		</td>
+	</tr>
+	
+	<tr>
+		<td class="tdbg1" colspan=2>
+			<span id="jsbtn">
+				<label><input type="checkbox" id="autoupdate" value="1"<?= $_COOKIE['plp_aupd'] ? " checked" : ""?>> Auto update CSS</label> | 
+				<button type="button" onclick="quickpreview(true)">Preview CSS</button> 
+			</span>
+			<input type="submit" name="submit" value="Preview All"> | 
+			Thread layout: <select name="tlayout">
+				<?= $tlayouts ?>
+			</select>
 		</td>
 	</tr>
 </table>
 </form>
-<?php
-/*
+
 <noscript><style>#jsbtn{display:none}</style></noscript>
-		
+
+	
 <script type="text/javascript">
-	// :rolleyes:
-	var css  = document.getElementById('css');
 	var user = <?= $_GET['id'] ?>;
+	
+	// Text area and destination CSS field
+	var css  = document.getElementById('css');
 	var css_dest  = document.getElementById('css0');
-	//var head = document.getElementById('postheader');
-	//var sign = document.getElementById('signature');
+	// For seamless scrolling
+	var postpreview  = document.getElementById('postpreview');
 	
-	var dest_css = document.getElementById('css0');
-	function quickpreview() {
-		css_dest.innerHTML = css.innerHTML.replace('/\.(top|side|main|cont)bar'+user+'/i', '.$1bar'+user+'_p0');
+	// Determine if to autoupdate the CSS
+	var autoupdate = document.getElementById('autoupdate');
+	
+	autoupdate.addEventListener('change', function() {
+		if (this.checked) {
+			css.addEventListener('input', quickpreview);
+			document.cookie = "plp_aupd=1"; 
+		} else {
+			css.removeEventListener('input', quickpreview);
+			document.cookie = "plp_aupd=; Max-Age=-99999999;";
+		}
+	});
+	
+	function quickpreview(scroll = false) {
+		css_dest.innerHTML = css.value.replace(new RegExp("\.(top|side|main|cont)bar"+user+"", 'gi'), '.$1bar'+user+'_p0');
+		if (scroll)
+			postpreview.scrollIntoView();
 	}
-	
 </script>
-*/
+<?php
 	
 	
 	

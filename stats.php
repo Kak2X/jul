9c9aeb0d342bb85f6bcb8a40e882e247eec52075
Kak <kak32x@gmail.com>
2018-11-05 01:14:23 +0100

    Split daily stats to own page
    Merging changes from https://github.com/Xkeeper0/jul/commit/3df3b5e09f3d520512c98a8b75d76ce6d646d077

--- stats.php
+++ stats.php
@@ -40,6 +40,7 @@
 			<li><a href='activity3.php'>Graph of total post count and posts per day</a></li>
 			<li><a href='activity3u.php'>Graph of active users per day</a></li>
 			<li><a href='avatar.php'>Mood avatars</a></li>
+			<li><a href='stats-daily.php'>Daily board-wide statistics</a></li>
 		</ul>
 	</td>
 	</tr>
@@ -101,46 +102,7 @@ echo //tblinfo('posts_text').
 	.tblinfo('dailystats')
 	.tblinfo('rendertimes');
 ?>	</table>
-  <br>
-  <table class='table'>
-	<tr><td class='tdbgh fonts center' colspan=9>Daily stats</td></tr>
-	<tr>
-		<td class='tdbgc fonts center'>Date</td>
-		<td class='tdbgc fonts center'>Total users</td>
-		<td class='tdbgc fonts center'>Total posts</td>
-		<td class='tdbgc fonts center'>Total threads</td>
-		<td class='tdbgc fonts center'>Total views</td>
-		<td class='tdbgc fonts center'>New users</td>
-		<td class='tdbgc fonts center'>New posts</td>
-		<td class='tdbgc fonts center'>New threads</td>
-		<td class='tdbgc fonts center'>New views</td>
-	</tr>
 <?php
-	$users 		= 0;
-	$posts 		= 0;
-	$threads	= 0;
-	$views		= 0;
-	$stats = $sql->query("SELECT * FROM dailystats ORDER BY id ASC");	// NOTE: Originally dailystats did not have an ID. Added due to InnoDB shenanigains.
-	while($day = $sql->fetch($stats)){
-		?>
-	<tr>
-		<td class='tdbg1 fonts center'><?=$day['date']?></td>
-		<td class='tdbg2 fonts center'><?=$day['users']?></td>
-		<td class='tdbg2 fonts center'><?=$day['posts']?></td>
-		<td class='tdbg2 fonts center'><?=$day['threads']?></td>
-		<td class='tdbg2 fonts center'><?=$day['views']?></td>
-		<td class='tdbg2 fonts center'><?=($day['users']-$users)?></td>
-		<td class='tdbg2 fonts center'><?=($day['posts']-$posts)?></td>
-		<td class='tdbg2 fonts center'><?=($day['threads']-$threads)?></td>
-		<td class='tdbg2 fonts center'><?=($day['views']-$views)?></td>
-	</tr>
-	<?php
-		$users 		= $day['users'];
-		$posts 		= $day['posts'];
-		$threads 	= $day['threads'];
-		$views 		= $day['views'];
-	}
-?></table><?php
 
 	pagefooter();
 	

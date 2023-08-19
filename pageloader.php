93209f04d032319db7c72ce0d743af1d2c973edb
Kak <kak32x@gmail.com>
2023-08-19 18:12:02 +0200

    Fix dumb exploit in pageloader and redone logic

--- pageloader.php
+++ pageloader.php
@@ -1,102 +1,119 @@
 <?php
 
+	/* 
+		Extension Page Loader
+		
+		This file is used as 404 handler, to load otherwise inaccessible files inside the /extensions folder.
+	*/
 	const LOADER_DEBUG = false;
 	
-	// Generic page loader
+	// Only include what we need. 
 	require "lib/routing.php";
 
-	// Get the root directory similarly
+	// Get the root directory relative to the htdocs root as normal
 	fetch_root($root, $boardurl);
-	$url = str_replace($boardurl."/", "", $_SERVER['REQUEST_URI']);
-
-	/*
-	// Alias of existing file in pages?
-	if (file_exists("pages/{$url}.php")) {
-	//	echo "\nOK";
-		die;
-	}*/
+	
+	// Get the relative URL by removing that aforemented root directory from $_SERVER['REQUEST_URI']
+	// Allowed URL formats:
+	// <extName>/[file][?<args>]
+	// <extName>[?<args>]
+	$_url = str_replace("{$boardurl}/", "", $_SERVER['REQUEST_URI']);
+	
+	// Remove arguments, if any
+	$_paramPos = strpos($_url, "?");
+	if ($_paramPos !== false) {
+		$_url = substr($_url, 0, $_paramPos);
+	}
+	
+	// Split URL parts
+	$_parts = explode("/", $_url, 2); // leave "/" in the file name
+	$_bypassInit = false;
+	$_extSubDir = "files/";
+	
+	if (count($_parts) === 1) {
+		/*if (file_exists("pages/{$_parts[0]}.php")) {
+			require "pages/{$_parts[0]}.php";
+			die;
+		}*/
+		$extName = $_parts[0];
+		$extFile = "index.php";
+		$_fileType = "php";
+	} else {
+		$extName = $_parts[0];
+		$extFile = $_parts[1];
 		
-	
-	
-	do {
-		// Parse the extension page format <extName>/<file> (ie: "news/new.php")
-		$extPos = strpos($url, "/");
-		if ($extPos === false) {
-			// Fallback when omitting the slash (ie: news?debug=1)
-			$extPos = strpos($url, "?");
-			if ($extPos === false) {
-				// double fallback. just use index page (ie: news)
-				$extName    = $url;
-				$extFile    = "index.php";
-				$fileType   = "php";
-				$loadConfig = false;
-				$bypassInit = false;
+		switch ($extFile) {
+			// Blank filename -> index page
+			case "":
+				$extFile = "index.php";
+				$_fileType = "php";
 				break;
-			}
-			$paramPos = $extPos + 1;
-		} else {
-			$paramPos = strpos($url, "?", $extPos);
+			// "config" -> configuration page. Must be always accessible.
+			case "config":
+				$extFile = "__config__.php";
+				$_virtualFile = "config";
+				$_extSubDir = "";
+				$_fileType = "php";
+				$_bypassInit = true;
+				break;
+			// Default behaviour
+			default:
+				if (in_array("..", explode("/", $extFile)))
+					die("Loader failure.");
+						
+				// Detect file extension (type)
+				$_typePos = strrpos($extFile, ".");
+				$_fileType = $_typePos !== false ? substr($extFile, $_typePos+1) : "";
+				
+				// Only php files are disallowed to run if the extension is disabled
+				$_bypassInit = $_fileType !== "php";
+				break;
 		}
-		
-		$extName = substr($url, 0, $extPos);
-		$extFile = $paramPos !== false
-			? substr($url, $extPos+1, $paramPos-($extPos+1))
-			: substr($url, $extPos+1);
-
-		// If the parameter wasn't passed, go by the index page (which is optional and may not exist)
-		if (!$extFile) {
-			$extFile    = "index.php";
-			$fileType   = "php";
-			$loadConfig = false;
-			$bypassInit = false;
-		// If the parameter matches this key, force load the config page, even if the extension is disabled.
-		// this is important as the config page should always be accessible
-		} else if ($extFile === "config") {
-			$fileType   = "php";
-			$loadConfig = true;
-			$bypassInit = true;
-		} else {
-			$fileType   = substr($extFile, -3);
-			$loadConfig = false;
-			$bypassInit = $fileType !== "php";
-		}
-	} while (false);
+	}
 		
 	if (LOADER_DEBUG) {
 		print "<pre>";
 		print "\nRoot: {$root}\nBoard Url: {$boardurl}";
-		print "\nURL: {$url}\nExtPos: {$extPos}\nParamPos: {$paramPos}";
-		print "\nExt Name: {$extName}\nExt File: {$extFile}";
-		print "\nExt Folder: ".(file_exists("extensions/{$extName}.abx") ? "OK" : "NG");
-		print "\nExt Exists: ".(in_array($extName, array_map('trim', file("extensions/init.dat")), true) ? "OK" : "NG");
-		print "\nLoadConfig: ".($loadConfig ? "TRUE" : "FALSE");
-		print "\nBypassInit: ".($bypassInit ? "TRUE" : "FALSE");
-		print "\nFile extension: {$fileType}";
-		print "\nWill load: 'extensions/{$extName}.abx/".($loadConfig ? "__config__.php" : "files/{$extFile}")."'";
+		print "\nURL: {$_url}\nParamPos: ".(isset($_paramPos) ? $_paramPos : "-")."\nPart Count: ".count($_parts);
+		print "\nExt Name: {$extName}\nExt File: {$extFile}\nExt SubDir: {$_extSubDir}\nVirtual File: ".(isset($_virtualFile) ? $_virtualFile : "");
+		print "\nExt Exists: ".(file_exists("extensions/{$extName}.abx") ? "OK" : "NG");
+		print "\nExt Enabled: ".(in_array($extName, array_map('trim', file("extensions/init.dat")), true) ? "OK" : "NG");
+		print "\nBypassInit: ".($_bypassInit ? "TRUE" : "FALSE");
+		print "\nFile extension: {$_fileType}\nTypePos: ".(isset($_typePos) ? $_typePos : "-");
+		print "\nWill load: 'extensions/{$extName}.abx/{$_extSubDir}{$extFile}'";
 		print "</pre>";
 	}
 	
 	// To save ourselves from trouble, if an extension is disabled we don't prevent access to the resources
 	// only php files will trigger the 404 page
-	
-	
-	if ($bypassInit || in_array($extName, array_map('trim', file("extensions/init.dat")), true)) {
-		$loadPath = "extensions/{$extName}.abx/".($loadConfig ? "__config__.php" : "files/{$extFile}");
+	if ($_bypassInit || in_array($extName, array_map('trim', file("extensions/init.dat")), true)) {
+		$loadPath = "extensions/{$extName}.abx/{$_extSubDir}{$extFile}";
 		if (file_exists($loadPath)) {
+			
+			// If it's a folder, try to redirct us there.
+			// This works alongside an htaccess rule to hide php files from there.
 			if (is_dir($loadPath)) {
 				header("Location: {$boardurl}/{$loadPath}");
 				die;
 			}
-			// :(
-			if ($fileType === "css") {
-				header("Content-type: text/css");
-			} else if ($fileType !== "php") {
-				header("Content-type: ".mime_content_type($loadPath)."");
+			
+			// Non-php files are loaded as data
+			if ($_fileType !== "php") {
+				if ($_fileType === "css") // not autodetected correctly
+					header("Content-type: text/css");
+				else
+					header("Content-type: ".mime_content_type($loadPath)."");
+				
+				readfile($loadPath);
+				die;
 			}
 			
-			$scriptpath = "$extName/$extFile"; // For forms
-			$meta['base'] = $boardurl."/"; // Enable base href tag to point to correct images/CSS
-			unset($url, $extPos, $loadConfig, $fileType);
+			$scriptpath = "{$extName}/".(isset($_virtualFile) ? $_virtualFile : $extFile); // For relative links to the current extension (with actionlink)
+			$meta['base'] = "{$boardurl}/"; // Enable base href tag to point to correct images/CSS
+			
+			// Explicitly destroy anything specific to the pageloader
+			unset($_url, $_paramPos, $_parts, $_bypassInit, $_extSubDir, $_fileType, $_typePos, $_virtualFile);
+			
 			require_once($loadPath);
 			die;
 		}

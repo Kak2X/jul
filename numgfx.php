b4db999f4a985e5395596ebff7c132b7ddb4a917
Kak <kak32x@gmail.com>
2023-08-29 15:26:21 +0200

    Fix all warnings from numgfx

--- numgfx.php
+++ numgfx.php
@@ -1,45 +1,50 @@
 <?php
 
-	error_reporting(0);
+	const TILE_W = 8;
+	const TILE_H = 8;
 	
-	$n = $_GET['n'];
-	$l = isset($_GET['l']) ? $_GET['l'] : 1;
-	$f = $_GET['f'];
+	$chars      = isset($_GET['n']) ? (string)$_GET['n'] : "";
+	$min_chars  = isset($_GET['l']) ? (int)$_GET['l'] : 1;
+	$file       = isset($_GET['f']) ? basename($_GET['f']) : "";
+	if (!$file) 
+		$file = 'numnes';
 	
-	$len = strlen($n.'');
+	// Always draw as many characters as the string length's
+	$text_len = strlen($chars);
 	
-	if($len < $l) {
-		$ofs = $l - $len;
-		$len = $l;
+	if ($text_len > $min_chars) {
+		// If the min width is less than that, make it match the string's length
+		$min_chars = $text_len;
+		$l_offset = 0;
 	} else {
-		$ofs = 0;
+		// Otherwise, right align the string
+		$l_offset = $min_chars - $text_len;
 	}
 	
-	if (!$f) $f = 'numnes';
+	$gfx = ImageCreateFromPNG("numgfx/{$file}.png");
+	$img = ImageCreate($min_chars * TILE_W, TILE_H);
 	
-	$gfx = ImageCreateFromPNG("numgfx/$f.png");
-	$img = ImageCreate($len * 8, 8);
+	// ???
+	//ImageCopy($img, $gfx, 0, 0, 104, 0, 1, 1);
 	
-	ImageCopy($img, $gfx, 0, 0, 104, 0, 1, 1);
-	for($i = 0; $i < $len; ++$i) {
-		switch($n[$i]){
-			case '/': $d=10; break;
-			case 'N': $d=11; break;
-			case 'A': $d=12; break;
-			case '-': $d=13; break;
-			default: $d = $n[$i];
+	// Print the characters one by one
+	for ($i = 0; $i < $text_len; ++$i) {
+		switch ($chars[$i]) {
+			case '/': $d = 10; break;
+			case 'N': $d = 11; break;
+			case 'A': $d = 12; break;
+			case '-': $d = 13; break;
+			default: $d = (int)$chars[$i];
 		}
-		ImageCopy($img, $gfx, ($i + $ofs) * 8, 0, $d * 8, 0, 8, 8);
+		ImageCopy($img, $gfx, ($i + $l_offset) * TILE_W, 0, $d * TILE_W, 0, TILE_W, TILE_H);
 	}
 	
 	Header('Content-type:image/png');
 	
-	if ($f == "numdeath") {
-		$ctp	= 1;
-	} else {
-		$ctp	= 0;
-	}
+	// ??? - This looks wrong
+	// $ctp = ($file == "numdeath");
+	$ctp = false;
+	
 	ImageColorTransparent($img,$ctp);
 	ImagePNG($img);
 	ImageDestroy($img);
-?>
